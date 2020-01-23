6448208bf9ef290a092af6a59a6f51e0510375ab
argush3 <argus@highwaythreesolutions.com>
2020-01-23 11:33:29 -0800

    Update action

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const core = require('@actions/core');
 const github = require('@actions/github');
-let request = require('request');
+let request = require('request-promise-native');
 
 var bodyData = {
         "audience": "api.atlassian.com", 
@@ -20,33 +20,62 @@ var options = {
 };
 
 
+async function getAccessToken() {
+    try {
+        const clientId = core.getInput('client-id');
+        const clientSecret = core.getInput('client-secret');
 
-try {
-    const clientId = core.getInput('client-id');
-    const clientSecret = core.getInput('client-secret');
+        bodyData.client_id = clientId;
+        bodyData.client_secret = clientSecret;
+        bodyData = JSON.stringify(bodyData);
+        options.body = bodyData;
 
-    bodyData.client_id = clientId;
-    bodyData.client_secret = clientSecret;
-    bodyData = JSON.stringify(bodyData);
-    options.body = bodyData;
+        // const payload = JSON.stringify(github.context.payload, undefined, 2)
+        // console.log(`The event payload: ${payload}`);
 
-    const payload = JSON.stringify(github.context.payload, undefined, 2)
-    // console.log(`The event payload: ${payload}`);
-
-    request(options, function (error, response, body) {
-        if (error) throw new Error(error);
-        console.log(
-            'Response getAccessToken: ' + response.statusCode + ' ' + response.statusMessage
-        );
-        console.log(body);
-
+        const response = await request(options);
+        console.log("response: ", response);
         core.setOutput("access-token", body.access_token);
-
-    });
-
-} catch (error) {
-    core.setFailed(error.message);
+    } catch (error) {
+        core.setFailed(error.message);
+    }
 }
+
+
+(async function () {
+    await getAccessToken();
+
+    console.log("finished retrieving access token");
+})();
+
+
+
+// try {
+//     const clientId = core.getInput('client-id');
+//     const clientSecret = core.getInput('client-secret');
+//
+//     bodyData.client_id = clientId;
+//     bodyData.client_secret = clientSecret;
+//     bodyData = JSON.stringify(bodyData);
+//     options.body = bodyData;
+//
+//     const payload = JSON.stringify(github.context.payload, undefined, 2)
+//     // console.log(`The event payload: ${payload}`);
+//
+//     request(options, function (error, response, body) {
+//         if (error) throw new Error(error);
+//         console.log(
+//             'Response getAccessToken: ' + response.statusCode + ' ' + response.statusMessage
+//         );
+//         console.log(body);
+//
+//         core.setOutput("access-token", body.access_token);
+//
+//     });
+//
+// } catch (error) {
+//     core.setFailed(error.message);
+// }
 
 
 
